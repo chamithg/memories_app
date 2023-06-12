348b30d2bf4f3d336c76287cfa85de308b007926
gamagegit <cgamage92@yahoo.com>
2023-06-11 22:44:27 -0700

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Main from "./main";
+import Form from "./components/createForm/createForm";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Main />
+      <Form />
     </div>
   );
 }
